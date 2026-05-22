4f97c9bebb4890d6082ab643e5bfb5d505e615d0
Borsha Devi <borshadevi27@gmail.com>
2026-05-22 23:06:44 +0600

    schema.sql file  add create table for students

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,10 @@
 CREATE TABLE students(
     student_id BIGINT PRIMARY KEY,
     name VARCHAR (350),
-    
+    age SMALLINT ,
+    gender VARCHAR(20),
+    department VARCHAR(150),
+    email VARCHAR(250),
+    phone_number INT,
+    address VARCHAR(400)
 )
