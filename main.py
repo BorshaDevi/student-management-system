b20f233706c517f844d2f50d089485fbb75d9b9b
Borsha Devi <borshadevi27@gmail.com>
2026-05-29 23:47:45 +0600

    added while loop and implement choice 1 for create student

--- main.py
+++ main.py
@@ -7,25 +7,31 @@ def menu():
     print("1. Add student")
     print("2. View Student")
 
+while True:
+    menu()
+    choice=input('Enter choice :')
 
-# create student
+    # create student
+    if choice=='1':
 
-name = input("Enter name: ")
-age = (input("Enter age: "))
-class_no = (input("Enter class no: "))
-roll = (input("Enter roll: "))
-gender = input("Enter gender: ")
-department = input("Enter department: ")
-address = input("Enter address: ")
-email = input("Enter email: ")
-phone_number = input("Enter phone: ")
-s1=Student(name,age,class_no,roll,gender,department,address,email,phone_number)
-stu=StudentDB()
-valid,msg=Validator(s1)
-if(valid):
-    stu.create_student(s1)
-else:
-   print(msg)
+        name = input("Enter name: ")
+        age = (input("Enter age: "))
+        class_no = (input("Enter class no: "))
+        roll = (input("Enter roll: "))
+        gender = input("Enter gender: ")
+        department = input("Enter department: ")
+        address = input("Enter address: ")
+        email = input("Enter email: ")
+        phone_number = input("Enter phone: ")
+        s1=Student(name,age,class_no,roll,gender,department,address,email,phone_number)
+        stu=StudentDB()
+        valid,msg=Validator(s1)
+        if(valid):
+            stu.create_student(s1)
+        else:
+           print(msg)
+
+
 # view student
 stu.view_students()
 
